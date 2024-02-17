605b4b34342fc5c068eb5066bade5aaf21309520
bariq <btali086@uottawa.ca>
2024-02-16 22:31:25 -0500

    Add: dates to transactions, and log in

--- script.js
+++ script.js
@@ -6,29 +6,35 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2023-11-18T21:31:17.178Z',
+    '2023-12-23T07:42:02.383Z',
+    '2023-01-28T09:15:04.904Z',
+    '2023-04-01T10:17:24.185Z',
+    '2023-05-08T14:11:59.604Z',
+    '2023-07-26T17:01:17.194Z',
+    '2023-07-28T23:36:17.929Z',
+    '2023-08-01T10:51:36.790Z',
+  ],
 };
 const account2 = {
   owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2023-11-01T13:15:33.035Z',
+    '2023-11-30T09:48:16.867Z',
+    '2023-12-25T06:04:23.907Z',
+    '2023-01-25T14:18:46.235Z',
+    '2023-02-05T16:33:06.386Z',
+    '2023-04-10T14:43:26.374Z',
+    '2023-06-25T18:49:59.371Z',
+    '2023-07-26T12:01:20.894Z',
+  ],
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -63,19 +69,28 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // ]);
 
 // console.log(movements[movements.length - 1]);
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (accs, sort = false) {
   const movs = sort
-    ? movements.slice(movements).sort((a, b) => a - b)
-    : movements;
+    ? accs.movements.slice().sort((a, b) => a - b)
+    : accs.movements;
   containerMovements.innerHTML = '';
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const date = new Date(accs.movementsDates[i]);
+    const year = date.getFullYear();
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const displayDates = `${day}/${month}/${year}`;
 
     const html = ` <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov.toFixed(2)} ＄</div>
+    <div class="movements__date">${displayDates}</div>
+
+     <div class="movements__value">${mov.toFixed(2)} ＄</div>
+
+     
   </div>
         
         `;
@@ -133,10 +148,8 @@ const createUsername = function (users) {
   });
 };
 
-createUsername(accounts);
-
 const updateUi = function (account) {
-  displayMovements(account.movements);
+  displayMovements(account);
 
   calcPrintBalance(account);
 
@@ -145,6 +158,8 @@ const updateUi = function (account) {
 
 // log in
 let currentAccount;
+
+createUsername(accounts);
 btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
   currentAccount = accounts.find(
@@ -160,6 +175,14 @@ btnLogin.addEventListener('click', function (e) {
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
     }`;
+
+    // create current date of logging in
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const day = `${now.getDate()}`.padStart(2, 0);
+
+    labelDate.textContent = `${day}/${month}/${year}`;
   }
 });
 
@@ -183,6 +206,8 @@ btnTransfer.addEventListener('click', function (e) {
 
     currentAccount.movements.push(-amount);
     receiverAccount.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAccount.movementsDates.push(new Date().toISOString)();
   }
   updateUi(currentAccount);
 });
@@ -194,6 +219,7 @@ btnLoan.addEventListener('click', function (e) {
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     console.log('okay');
     currentAccount.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
   } else {
     alert(
       'Amount requested exceeds the limit, you are allowed to request only 100% above your largest deposit'
